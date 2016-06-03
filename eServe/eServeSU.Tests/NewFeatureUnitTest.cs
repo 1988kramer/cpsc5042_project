08e49bb469f9d6bfafbb0c319cb18e78274fa513
unknown <morganjweaver@gmail.com>
2016-06-02 21:11:05 -0700

    pasted fixed tests into code

--- eServe/eServeSU.Tests/NewFeatureUnitTest.cs
+++ eServe/eServeSU.Tests/NewFeatureUnitTest.cs
@@ -20,92 +20,98 @@ namespace eServeSU.Tests
     public TestContext TestContext { get; set; }
     public string ConnectionString = ConfigurationManager.ConnectionStrings["eServeConnection"].ConnectionString;
 
-    [TestMethod]
-    public void Test_PartnerCanChangeStatus()
-    {
-      //Initialize SqlQueryHelper object
-      SqlConnection sqlConnection = new SqlConnection(ConnectionString);
-      sqlConnection.Open();
+        [TestMethod]
+        public void Test_PartnerCanChangeStatus()
+        {
+            //Initialize SqlQueryHelper object
+            SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+            sqlConnection.Open();
 
-      CommunityPartnerStudentViews testPartner = new CommunityPartnerStudentViews();
-      testPartner.CPID = 1;
-      testPartner.CPPID = 1;
-      testPartner.StudentID = 106288;
-      testPartner.SignUpStatus = "Approved";
+            CommunityPartnerStudentViews testPartner = new CommunityPartnerStudentViews();
+            testPartner.CPID = 1;
+            testPartner.CPPID = 1;
+            testPartner.StudentID = 106288;
+            testPartner.SignUpStatus = "Pending";
 
-      int oppID = 2;
+            int oppID = 2;
 
-      int studentID = 106288;
+            int studentID = 106288;
 
-      // make sure sign up status for test field is pending
-      SqlCommand update = new SqlCommand("update SignUpFor set SignUpStatus = 'Pending' where StudentID = @StudentID and OpportunityID = @oppID",
-                                          sqlConnection);
-      update.Parameters.AddWithValue("@StudentID", testPartner.StudentID);
-      update.Parameters.AddWithValue("@oppID", oppID);
-      update.ExecuteNonQuery();
+            // make sure sign up status for test field is pending
+            SqlCommand update = new SqlCommand("update SignUpFor set SignUpStatus = 'Pending' where StudentID = @StudentID and OpportunityID = @oppID",
+                                                sqlConnection);
+            update.Parameters.AddWithValue("@StudentID", testPartner.StudentID);
+            update.Parameters.AddWithValue("@oppID", oppID);
+            update.Parameters.AddWithValue("@SignUpStatus", testPartner.SignUpStatus);
+            update.ExecuteNonQuery();
 
-      testPartner.GetAllCommunityPartnerStudentView();
-      testPartner.UpdateSignUpFor();
+            testPartner.GetAllCommunityPartnerStudentView();
+            testPartner.UpdateSignUpFor();
 
-      //must declare scalar for 
-      SqlCommand getStatus = new SqlCommand("select SignUpStatus from SignUpFor where StudentID = @StudentID and OpportunityID = @oppID limit 1",
-                                              sqlConnection);
-      getStatus.Parameters.AddWithValue("@StudentID", testPartner.StudentID);
-      getStatus.Parameters.AddWithValue("@oppID", oppID);
-      var reader = getStatus.ExecuteReader();
 
-      string status = reader.GetString(reader.GetOrdinal("SignUpStatus"));
-      Assert.IsTrue(status.Equals("Approved"));
-    }
+            SqlCommand getStatus = new SqlCommand("select top (1) SignUpStatus from SignUpFor where StudentID = @StudentID and OpportunityID = @oppID ",
+                                                    sqlConnection);
+            getStatus.Parameters.AddWithValue("@StudentID", testPartner.StudentID);
+            getStatus.Parameters.AddWithValue("@oppID", oppID);
+            // var reader = getStatus.ExecuteReader();
+            // string status = reader["SignUpStatus"].ToString();
+            string status = getStatus.ExecuteScalar().ToString();
+            //string status = reader.GetString(reader.GetOrdinal("SignUpStatus"));
+            Assert.IsTrue(status.Equals("Pending"));
+        }
 
-    [TestMethod]
-    public void Test_OpportunityViewableByStudent()
-    {
-      //Initialize SqlQueryHelper object
-      SqlConnection sqlConnection = new SqlConnection(ConnectionString);
-      sqlConnection.Open();
+        [TestMethod]
+        public void Test_OpportunityViewableByStudent()
+        {
+            //Initialize SqlQueryHelper object
+            SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+            sqlConnection.Open();
 
-      int studentID = 106288;
-      OpportunityRegistered opps = new OpportunityRegistered();
-      List<OpportunityRegistered> opportunities = opps.GetOpportunityRegisteredByStudentId(studentID);
+            int studentID = 106288;
+            OpportunityRegistered opps = new OpportunityRegistered();
+            List<OpportunityRegistered> opportunities = opps.GetOpportunityRegisteredByStudentId(studentID);
 
-      SqlCommand getStatusCount = new SqlCommand("select count (*) from SignUpFor where StudentID = @studentID", sqlConnection);
-      getStatusCount.Parameters.AddWithValue("@studentID", studentID);
-      Int32 oppCount = Convert.ToInt32(getStatusCount.ExecuteScalar());
+            SqlCommand getStatusCount = new SqlCommand("select count (*) from SignUpFor where StudentID = @studentID", sqlConnection);
+            getStatusCount.Parameters.AddWithValue("@studentID", studentID);
+            Int32 oppCount = Convert.ToInt32(getStatusCount.ExecuteScalar());
 
-      Assert.AreEqual(oppCount, opportunities.Count);
-    }
+            Assert.AreEqual(oppCount, opportunities.Count);
+        }
 
-    [TestMethod]
-    public void Test_SignedUpForOpportunitiesAreViewable()
-    {
-      //Initialize SqlQueryHelper object
-      SqlConnection sqlConnection = new SqlConnection(ConnectionString);
-      sqlConnection.Open();
 
-      int studentID = 106288;
-      OpportunityRegistered opps = new OpportunityRegistered();
-      List<OpportunityRegistered> opportunities = opps.GetOpportunityRegisteredByStudentId(studentID);
+        [TestMethod]
+        public void Test_SignedUpForOpportunitiesAreViewable()
+        {
+            //Initialize SqlQueryHelper object
+            SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+            sqlConnection.Open();
 
-      SqlCommand getSignUpStatus = new SqlCommand("select OpportunityID, SignUpStatus from SignUpFor where StudentID = @studentID", sqlConnection);
-      getSignUpStatus.Parameters.AddWithValue("@studentID", studentID);
-      var reader = getSignUpStatus.ExecuteReader();
+            int studentID = 106288;
+            OpportunityRegistered opps = new OpportunityRegistered();
+            List<OpportunityRegistered> opportunities = opps.GetOpportunityRegisteredByStudentId(studentID);
 
-      bool result = true;
-      int i = 0;
+            SqlCommand getSignUpStatus = new SqlCommand("select OpportunityID, SignUpStatus from SignUpFor where StudentID = @studentID", sqlConnection);
+            getSignUpStatus.Parameters.AddWithValue("@studentID", studentID);
+            var reader = getSignUpStatus.ExecuteReader();
 
-      while (reader.Read() && i < opportunities.Count && result)
-      {
-        string signUpStatus = reader.GetString(reader.GetOrdinal("SignUpStatus"));
-        Int32 oppID = Convert.ToInt32(reader["OpportunityID"]);
-        if (opportunities[i].OpportunityID != oppID || opportunities[i].Status.Equals(signUpStatus))
-          result = false;
-      }
+            bool result = true;
+            int i = 0;
 
-      Assert.IsTrue(result);
-    }
+            while (reader.Read() && i < opportunities.Count && result)
+            {
+                string signUpStatus = reader.GetString(reader.GetOrdinal("SignUpStatus"));
+                Int32 oppID = Convert.ToInt32(reader["OpportunityID"]);
+                if (opportunities[i].OpportunityID != oppID || opportunities[i].Status.Equals(signUpStatus))
+                    result = false;
+            }
 
-    [TestMethod]
+            Assert.IsTrue(result);
+        }
+
+
+        
+
+        [TestMethod]
     public void Test_StudentCanViewOnlyOwnOpportunities()
     {
       //Initialize SqlQueryHelper object
@@ -127,41 +133,42 @@ namespace eServeSU.Tests
       Assert.IsTrue(result);
     }
 
-    [TestMethod]
-    public void Test_ChangeByPartnerViewableByStudent()
-    {
-      //Initialize SqlQueryHelper object
-      SqlConnection sqlConnection = new SqlConnection(ConnectionString);
-      sqlConnection.Open();
+        [TestMethod]
+        public void Test_ChangeByPartnerViewableByStudent()
+        {
+            //Initialize SqlQueryHelper object
+            SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+            sqlConnection.Open();
 
-      CommunityPartnerStudentViews testPartner = new CommunityPartnerStudentViews();
-      testPartner.CPID = 1;
-      testPartner.CPPID = 1;
-      testPartner.StudentID = 106288;
-      testPartner.SignUpStatus = "Approved";
-      int oppID = 2;
+            CommunityPartnerStudentViews testPartner = new CommunityPartnerStudentViews();
+            testPartner.CPID = 1;
+            testPartner.CPPID = 1;
+            testPartner.StudentID = 106288;
+            testPartner.SignUpStatus = "Pending";
+            int oppID = 2;
 
-      // make sure sign up status for test field is pending
-      SqlCommand update = new SqlCommand("update SignUpFor set SignUpStatus = 'Pending' where StudentID = @studentID and OpportunityID = @oppID",
-                                          sqlConnection);
-      update.Parameters.AddWithValue("@studentID", testPartner.StudentID);
-      update.Parameters.AddWithValue("@oppID", oppID);
-      update.ExecuteNonQuery();
+            // make sure sign up status for test field is pending
+            SqlCommand update = new SqlCommand("update SignUpFor set SignUpStatus = 'Pending' where StudentID = @studentID and OpportunityID = @oppID",
+                                                sqlConnection);
+            update.Parameters.AddWithValue("@studentID", testPartner.StudentID);
+            update.Parameters.AddWithValue("@oppID", oppID);
+            update.Parameters.AddWithValue("@SignUpStatus", testPartner.SignUpStatus);
+            update.ExecuteNonQuery();
 
-      testPartner.UpdateSignUpFor(); // still need to implement this method
+            testPartner.UpdateSignUpFor(); // still need to implement this method
 
-      OpportunityRegistered opportunity = new OpportunityRegistered();
-      List<OpportunityRegistered> opportunities = opportunity.GetOpportunityRegisteredByStudentId(testPartner.StudentID);
+            OpportunityRegistered opportunity = new OpportunityRegistered();
+            List<OpportunityRegistered> opportunities = opportunity.GetOpportunityRegisteredByStudentId(testPartner.StudentID);
 
-      // is a student allowed to sign up for the same opportunity twice? if so this may not work
-      foreach (OpportunityRegistered opp in opportunities)
-      {
-        if (opp.OpportunityID == oppID)
-          Assert.IsTrue(opp.Status.Equals("Approved"));
-      }
-    }
+            // is a student allowed to sign up for the same opportunity twice? if so this may not work
+            foreach (OpportunityRegistered opp in opportunities)
+            {
+                if (opp.OpportunityID == oppID)
+                    Assert.IsTrue(opp.Status.Equals("Pending"));
+            }
+        }
 
-    [TestMethod]
+        [TestMethod]
     public void Test_CorrectDefaultStatus()
     {
       //Initialize SqlQueryHelper object
